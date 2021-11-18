b2d274c3ae1d50046389fc853ef5da18033983c8
Krishna Priya <krishnamath27@gmail.com>
2021-11-18 13:08:24 +0530

    Messaging app

--- app/src/main/java/com/example/messagemax/MainActivity.java
+++ app/src/main/java/com/example/messagemax/MainActivity.java
@@ -4,11 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -26,5 +27,32 @@ public class MainActivity extends AppCompatActivity {
     public void openSignUpPage(){
         Intent intent = new Intent(this, SignUpPage.class);
         startActivity(intent);
+    }
+
+
+    boolean pressed = false;
+
+    @Override
+    public void onBackPressed() {
+        if(pressed){
+            finishAffinity();
+            System.exit(0);
+        }
+        else {
+            Toast.makeText(getApplicationContext(),
+                    "Click back again to exit",
+                    Toast.LENGTH_SHORT).show();
+            pressed=true;
+        }
+
+        Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                pressed = false;
+            }
+        };
+
+        new Handler().postDelayed(runnable, 2000);
+
     }
 }
---
+++ app/src/main/java/com/example/messagemax/cnct_page.java
@@ -0,0 +1,14 @@
+package com.example.messagemax;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class cnct_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cnct_page);
+    }
+}
